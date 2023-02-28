8e87031eee8ef18d86e1a19f4240065c3565551c
minckan <mymy317@naver.com>
2023-02-28 19:12:15 +0900

    병합오류 수정

--- TwiterApp/Controller/Authentication/RegistrationController.swift
+++ TwiterApp/Controller/Authentication/RegistrationController.swift
@@ -125,21 +125,9 @@ class RegistrationController : UIViewController {
             
             self.dismiss(animated: true)
         }
-
-            
-            guard let uid = result?.user.uid else {return}
-            let values = ["email": email, "username": username, "fullname": fullname]
-            
-            let ref = Database.database().reference().child("users").child(uid)
-            
-            ref.updateChildValues(values) { error, ref in
-                print("DEBUG: Successfully updated user information...")
-            }
-        }
-                
+        
 
     }
-    
     // MARK: - Helpers
     func configureUI() {
         view.backgroundColor = .twitterBlue
