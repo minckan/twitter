9bce89e03535165cfdc2fbe34573089a8a02d787
minckan <mymy317@naver.com>
2023-03-17 00:33:01 +0900

    Add TwwetCell UI

--- TwiterApp/View/TweetCell.swift
+++ TwiterApp/View/TweetCell.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class TweetCell : UICollectionViewCell {
+    
+    // MARK: - Properties
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.setDimensions(width: 48, height: 48)
+        iv.layer.cornerRadius = 48/2
+        iv.backgroundColor = .twitterBlue
+        return iv
+    }()
+    
+    private let captionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let infoLabel = UILabel()
+    
+    
+    // MARK: - Lifecycle
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .red
+        backgroundColor = .white
+        addSubview(profileImageView)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    // MARK: - Selectors
+    
+    // MARK: - Helpers
 }
