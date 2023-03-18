58c79d0d03601cd81dc6e12c1ebac535209bf587
minckan <mymy317@naver.com>
2023-03-18 21:14:49 +0900

    TweetCellDelegate Protocol 추가

--- TwiterApp/Controller/FeedController.swift
+++ TwiterApp/Controller/FeedController.swift
@@ -78,7 +78,7 @@ extension FeedController {
     }
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! TweetCell
-            
+        cell.delegate = self
         cell.tweet = tweets[indexPath.row]
         
         return cell
@@ -91,4 +91,15 @@ extension FeedController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: view.frame.width, height: 120)
     }
+}
+
+// MARK: - TweetCellDelegate
+
+extension FeedController: TweetCellDelegate {
+    func handleProfileImageTapped() {
+        let controller = ProfileController(collectionViewLayout: UICollectionViewFlowLayout())
+        navigationController?.pushViewController(controller, animated: true)
+    }
+    
+    
 }
---
+++ TwiterApp/Controller/ProfileController.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileController.swift
+//  TwiterApp
+//
+//  Created by 강민주 on 2023/03/18.
+//
+
+import UIKit
+
+class ProfileController : UICollectionViewController {
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .red
+    }
+    
+    // MARK: - Selectors
+    
+    // MARK: - Helpers
+}
