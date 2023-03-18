52e5fe7d2cbce558957470d4a0b7638318cf0ebe
minckan <mymy317@naver.com>
2023-03-18 23:37:24 +0900

    Add Profile Filter Bar: collection view를 만들고 cell을 추가, 애니메이션도 추가

--- TwiterApp/View/ProfileHeader.swift
+++ TwiterApp/View/ProfileHeader.swift
@@ -10,6 +10,8 @@ import UIKit
 class ProfileHeader:UICollectionReusableView {
     // MARK: - Properties
     
+    private let filterBar = ProfileFilterView()
+    
     private lazy var containerView: UIView = {
         let view = UIView()
         view.backgroundColor = .twitterBlue
@@ -73,9 +75,18 @@ class ProfileHeader:UICollectionReusableView {
         return label
     }()
     
+    private let underlineView : UIView = {
+        let view = UIView()
+        view.backgroundColor = .twitterBlue
+        return view
+    }()
+    
     // MARK: - Lifecycle
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        filterBar.delegate = self
+        
         addSubview(containerView)
         containerView.anchor(top: topAnchor, left: leftAnchor, right: rightAnchor, height: 108)
         
@@ -96,6 +107,12 @@ class ProfileHeader:UICollectionReusableView {
         
         addSubview(userDetailStack)
         userDetailStack.anchor(top: profileImageView.bottomAnchor, left: leftAnchor, right: rightAnchor,paddingTop: 8, paddingLeft: 12, paddingRight: 12)
+        
+        addSubview(filterBar)
+        filterBar.anchor(left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, height: 50)
+        addSubview(underlineView)
+        underlineView.anchor(left: leftAnchor, bottom: bottomAnchor, width: frame.width / 3, height: 2)
+     
     }
     
     required init?(coder: NSCoder) {
@@ -114,4 +131,16 @@ class ProfileHeader:UICollectionReusableView {
     
     // MARK: - Helpers
     
+}
+// MARK: - ProfileFilterViewDelegate
+extension ProfileHeader: ProfileFilterViewDelegate {
+    func filterView(_ view: ProfileFilterView, didSelect indexPath: IndexPath) {
+        guard let cell = view.collectionView.cellForItem(at: indexPath) as? ProfileFilterCell else {return}
+        
+        let xPosition = cell.frame.origin.x
+        UIView.animate(withDuration: 0.3) {
+            self.underlineView.frame.origin.x = xPosition
+        }
+      
+    }
 }
